8713a851bf414bb10c2b6a6b6f2553e24604347f
unknown <kenneth.restauro@g.msuiit.edu.ph>
2026-09-22 14:28:25 +0800

    Uncommenting Functions

--- DAC/DAC.ino
+++ DAC/DAC.ino
@@ -1,22 +1,21 @@
 #define DAC_PIN 25
 
 void setup() {
-  dacWrite(DAC_PIN, 255);
 }
 
 void loop() {
-  //dacWrite(DAC_PIN, 0);
+  dacWrite(DAC_PIN, 0);
   delay(2000);
 
-  //dacWrite(DAC_PIN, 64);
+  dacWrite(DAC_PIN, 64);
   delay(2000);
 
-  //dacWrite(DAC_PIN, 128);
+  dacWrite(DAC_PIN, 128);
   delay(2000);
 
-  //dacWrite(DAC_PIN, 192);
+  dacWrite(DAC_PIN, 192);
   delay(2000);
 
-  //dacWrite(DAC_PIN, 255);
+  dacWrite(DAC_PIN, 255);
   delay(2000);
 }
