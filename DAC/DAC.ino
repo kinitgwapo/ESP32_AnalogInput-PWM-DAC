6d03bd0b5ccc17e38df7e0c8ce00feb744c33830
unknown <kenneth.restauro@g.msuiit.edu.ph>
2026-09-22 14:27:05 +0800

    Initialized DAC

---
+++ DAC/DAC.ino
@@ -0,0 +1,22 @@
+#define DAC_PIN 25
+
+void setup() {
+  dacWrite(DAC_PIN, 255);
+}
+
+void loop() {
+  //dacWrite(DAC_PIN, 0);
+  delay(2000);
+
+  //dacWrite(DAC_PIN, 64);
+  delay(2000);
+
+  //dacWrite(DAC_PIN, 128);
+  delay(2000);
+
+  //dacWrite(DAC_PIN, 192);
+  delay(2000);
+
+  //dacWrite(DAC_PIN, 255);
+  delay(2000);
+}
